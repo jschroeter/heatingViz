081668db9af3ad04a487e1b2aaaa27daf790c984
Jakob Schröter <487046+jschroeter@users.noreply.github.com>
2022-05-03 22:53:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,173 @@
-async function getData (sheetName) {
+async function getData(sheetName) {
   let data = [];
-  await fetch("https://opensheet.elk.sh/18jTY-XRlSNsuitDiuXnzA-ldyG8hjSkQP6Ud6vt0_ko/" + sheetName).then((res) => res.json())
+  await fetch(
+    'https://opensheet.elk.sh/18jTY-XRlSNsuitDiuXnzA-ldyG8hjSkQP6Ud6vt0_ko/' +
+      sheetName
+  )
+    .then((res) => res.json())
     .then((rawData) => {
-      rawData.forEach(row => {
+      rawData.forEach((row) => {
         let [date, time] = row.Zeit.split(' ');
         let [day, month, year] = date.split('.');
         let [hour, minute, second] = time.split(':');
         let dateObject = new Date(year, month - 1, day, hour, minute, second);
         data.push([dateObject, parseFloat(row.Watt)]);
       });
-  });
+    });
   return data;
-};
+}
 
-async function init () {
+async function init() {
   function buildSeries(name, color, data) {
     return {
       name,
       type: 'line',
       symbol: 'none',
       sampling: 'lttb',
-      connectNulls: true,
+      step: 'start',
       itemStyle: {
-        color
+        color,
+      },
+      lineStyle: {
+        width: 0,
       },
       areaStyle: {},
-      data
+      data,
     };
   }
-  let [warmwasser, heizung, solar] = await Promise.all([getData('Warmwasser'), getData('Heizung'), getData('Solar')]);
+  let [warmwasser, heizung, solar] = await Promise.all([
+    getData('Warmwasser'),
+    getData('Heizung'),
+    getData('Solar'),
+  ]);
+  let warmwasserSeries = buildSeries('Warmwasser', 'blue', warmwasser);
+  warmwasserSeries.markLine = {
+      lineStyle: {
+        color: 'black',
+        width: 7
+      },
+      label: {
+        formatter: '{b}' // bug in echarts?
+      },
+      data: [
+        {
+          name: 'Eco-Mode aktiviert',
+          xAxis: '2022-04-24 18:00:00'
+        }
+      ]
+  };
+
+  warmwasserSeries.markArea = {
+    itemStyle: {
+      color: 'rgba(255, 0, 0, 0.1)'
+    },
+    data: [
+      [
+        {
+          name: 'Unnötige Speicherladung',
+          xAxis: '2022-04-20 22:44:00'
+        },
+        {
+          xAxis: '2022-04-20 23:25:00'
+        }
+      ],
+      [
+        {
+          name: 'Unnötige Speicherladung',
+          xAxis: '2022-04-21 23:04:00'
+        },
+        {
+          xAxis: '2022-04-21 23:34:00'
+        }
+      ],
+      [
+        {
+          name: 'Unnötige Speicherladung',
+          xAxis: '2022-04-22 21:52:00'
+        },
+        {
+          xAxis: '2022-04-22 22:32:00'
+        }
+      ],
+      [
+        {
+          name: 'Solarthermie Fehlfunktion',
+          xAxis: '2022-05-01 09:52:00'
+        },
+        {
+          xAxis: '2022-05-01 17:43:00'
+        }
+      ],
+      [
+        {
+          name: 'Solarthermie Fehlfunktion',
+          xAxis: '2022-05-02 08:44:00'
+        },
+        {
+          xAxis: '2022-05-02 17:43:00'
+        }
+      ]
+    ]
+    
+  };
+
   let series = [
-    buildSeries('Warmwasser', 'blue', warmwasser),
-    buildSeries('Heizung', 'red', heizung),
+    warmwasserSeries,
     buildSeries('Solar', 'orange', solar),
+    buildSeries('Gas', 'grey', heizung),
   ];
-  
+
+
+
+  let zoomDateEnd = new Date();
+  let zoomDateStart = new Date(zoomDateEnd);
+  zoomDateStart.setDate(zoomDateEnd.getDate() - 3);
+
   let option = {
     tooltip: {
       trigger: 'axis',
       position: function (pt) {
         return [pt[0], '10%'];
-      }
+      },
     },
     toolbox: {
       feature: {
         dataZoom: {
-          yAxisIndex: 'none'
+          yAxisIndex: 'none',
         },
-        restore: {}
-      }
+        restore: {},
+      },
+    },
+    legend: {
+      data: ['Warmwasser', 'Solar', 'Gas'],
     },
     xAxis: {
       type: 'time',
-      boundaryGap: false
+      boundaryGap: false,
     },
     yAxis: {
       type: 'value',
-      boundaryGap: false
+      boundaryGap: false,
+      axisLabel: {
+        formatter: '{value} W',
+      },
     },
     dataZoom: [
       {
         type: 'inside',
-        start: 0,
-        end: 10
+        startValue: zoomDateStart,
+        endValue: zoomDateEnd,
       },
       {
-        start: 0,
-        end: 10
-      }
+        startValue: zoomDateStart,
+        endValue: zoomDateEnd,
+      },
     ],
-    series
+    series,
   };
 
   var myChart = echarts.init(document.getElementById('chart-container'));
   myChart.setOption(option);
   window.addEventListener('resize', myChart.resize);
-};
+}
 init();
